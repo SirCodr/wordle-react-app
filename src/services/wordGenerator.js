cb474a75b4750c6e20add750c73c9058082b9bcd
SirCodr <jotarestrepo01@gmail.com>
2023-06-18 15:42:52 -0500

    Refactor system message in word generator function

--- src/services/wordGenerator.js
+++ src/services/wordGenerator.js
@@ -1,15 +1,15 @@
 import { Configuration, OpenAIApi } from "openai"
 
 const configuration = new Configuration({
-    apiKey: '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',
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
     // apiKey: process.env.OPENAI_API_KEY,
 })
 
 export const fetchRandomWordByCategory = async (category) => {
   const openai = new OpenAIApi(configuration)
   const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo-16k-0613",
-    messages: [{ role: "user", content: `Hablando de ${category}, dime una palabra de 5 letras que no tenga tildes y sin puntos`}],
+    model: "gpt-3.5-turbo-16k",
+    messages: [{ role: "system", content: `generame una palabra en español, de máximo 6 letras sin tildes; que encaje en la categoria de '${category}'; que represente un objeto, acción o actividad. La respuesta solo debe retornar la palabra`}],
 })
 return response.data?.choices[0]?.message?.content
 }
